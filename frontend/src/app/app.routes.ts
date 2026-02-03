7925e213f278ea1b30d2b87b04d32da85d89856d
rhayssakramer <rhayssakramer@hotmail.com>
2026-02-03 00:25:55 -0300

    feat: ajuste geral de rotas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from '../pages/index.component';
+import { HomeComponent } from './pages/home.component';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: '' }
 ];
