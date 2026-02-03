05e11f31e33b54ea053d9b64cf7be1a3f987e9a4
rhayssakramer <rhayssakramer@hotmail.com>
2026-02-03 00:23:50 -0300

    feat: criação de componet home e ajuste de rota

--- frontend/src/pages/index.component.ts
+++ frontend/src/pages/index.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { ParticlesComponent } from '../app/components/particles.component';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [CommonModule, ParticlesComponent],
+  imports: [CommonModule, RouterLink, ParticlesComponent],
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
