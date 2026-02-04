d2718bd6999efdec62d1ef82a32339d5e6989900
rhayssakramer <rhayssakramer@hotmail.com>
2026-02-04 17:36:11 -0300

    feat: inserindo informações no resume.component

--- frontend/src/app/components/resume/resume.component.ts
+++ frontend/src/app/components/resume/resume.component.ts
@@ -8,4 +8,31 @@ import { CommonModule } from '@angular/common';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  expandedCards: { [key: string]: boolean } = {
+    education: false,
+    experience: false,
+    certifications: false,
+    certificates: false
+  };
+
+  // Quantidade de itens em cada card
+  cardItemsCount: { [key: string]: number } = {
+    education: 1,
+    experience: 3,
+    certifications: 3,
+    certificates: 9
+  };
+
+  toggleCard(cardName: string) {
+    this.expandedCards[cardName] = !this.expandedCards[cardName];
+  }
+
+  isExpanded(cardName: string): boolean {
+    return this.expandedCards[cardName];
+  }
+
+  hasMultipleItems(cardName: string): boolean {
+    return this.cardItemsCount[cardName] > 1;
+  }
+}
