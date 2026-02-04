592b64c5e408b2d1ada7dfa694c8ea0c77a9bfee
rhayssakramer <rhayssakramer@hotmail.com>
2026-02-04 02:29:23 -0300

    fix: ajustes de alinhamento responssividade

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ParticlesComponent } from '../../components/particles/particles.component';
+import { ProfileCardComponent } from '../../components/profile-card/profile-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, ParticlesComponent],
+  imports: [CommonModule, ParticlesComponent, ProfileCardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
