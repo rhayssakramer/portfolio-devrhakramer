868dedf1af3eb81cf58a69f29bd0530133fd7cd7
rhayssakramer <rhayssakramer@hotmail.com>
2026-05-21 01:05:02 -0300

    fix: ajustar padding do card e adicionar novos projetos ao portfólio

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -92,6 +92,26 @@ export class HomeComponent {
       link: 'https://github.com/rhayssakramer/microsoft-certification',
       linkText: 'Projeto de Materiais de estudos para o Microsoft Certification',
       description: 'Guia para contribuir com os estudos das Certificações da Microsoft.'
+    },
+    {
+      svgViewBox: '0 0 24 24',
+      svgFill: 'white',
+      svgType: 'path',
+      svgContent: 'M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.2 11.39.6.11.82-.26.82-.58 0-.29-.01-1.06-.02-2.08-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.34-1.76-1.34-1.76-1.09-.75.08-.73.08-.73 1.2.08 1.83 1.23 1.83 1.23 1.07 1.83 2.8 1.3 3.48.99.11-.78.42-1.3.76-1.6-2.66-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.23-3.22-.12-.3-.54-1.52.11-3.18 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 0 1 3-.4c1.02 0 2.05.14 3 .4 2.3-1.55 3.3-1.23 3.3-1.23.65 1.66.23 2.88.11 3.18.77.84 1.23 1.91 1.23 3.22 0 4.61-2.81 5.63-5.49 5.93.43.37.81 1.1.81 2.22 0 1.6-.01 2.89-.01 3.28 0 .32.21.69.82.57C20.56 21.78 24 17.3 24 12 24 5.37 18.63 0 12 0z',
+      title: 'Codex',
+      link: 'https://github.com/rhayssakramer/codex',
+      linkText: 'Acesse o repositório Codex no GitHub',
+      description: 'Codex é um projeto inovador para compartilhar conhecimento, dicas e soluções práticas de desenvolvimento.'
+    },
+    {
+      svgViewBox: '0 0 24 24',
+      svgFill: 'white',
+      svgType: 'path',
+      svgContent: 'M12 0C5.37 0 0 5.37 0 12c0 5.3 3.44 9.8 8.2 11.39.6.11.82-.26.82-.58 0-.29-.01-1.06-.02-2.08-3.34.73-4.04-1.61-4.04-1.61-.55-1.39-1.34-1.76-1.34-1.76-1.09-.75.08-.73.08-.73 1.2.08 1.83 1.23 1.83 1.23 1.07 1.83 2.8 1.3 3.48.99.11-.78.42-1.3.76-1.6-2.66-.3-5.47-1.33-5.47-5.93 0-1.31.47-2.38 1.23-3.22-.12-.3-.54-1.52.11-3.18 0 0 1.01-.32 3.3 1.23a11.5 11.5 0 0 1 3-.4c1.02 0 2.05.14 3 .4 2.3-1.55 3.3-1.23 3.3-1.23.65 1.66.23 2.88.11 3.18.77.84 1.23 1.91 1.23 3.22 0 4.61-2.81 5.63-5.49 5.93.43.37.81 1.1.81 2.22 0 1.6-.01 2.89-.01 3.28 0 .32.21.69.82.57C20.56 21.78 24 17.3 24 12 24 5.37 18.63 0 12 0z',
+      title: 'Portfolio DevRhaKramer',
+      link: 'https://github.com/rhayssakramer/portfolio-devrhakramer',
+      linkText: 'Acesse o repositório do Portfolio DevRhakramer no GitHub',
+      description: 'Seu portfólio profissional com Angular, TypeScript e JavaScript. Mostra seus projetos, habilidades e experiências.'
     }
   ];
 
